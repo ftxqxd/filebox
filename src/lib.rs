513eeae4ff869d8acedf00aa45abf836de9497c4
P1start <rewi-github@whanau.org>
2014-08-31 12:47:38 +1200

    more docs; tweak tests; add delete() method

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,8 @@
 #![feature(unsafe_destructor)]
 
-//! A magic box that writes to a file instead of memory.
+//! A box that writes to a file instead of memory.
 //!
-//! Example:
+//! # Example
 //!
 //! ```rust
 //! extern crate filebox;
@@ -26,17 +26,18 @@ extern crate serialize;
 
 use std::default::Default;
 use std::str;
-use std::io;
-use std::io::{File, IoError};
-use serialize::{Decoder, Decodable, Encoder, Encodable};
-use serialize::json;
+use std::io::{mod, fs, File, IoError, IoResult};
+use serialize::{json, Decoder, Decodable, Encoder, Encodable};
 
+/// A box that writes to a file when dropped, and reads from a file when created.
 pub struct FileBox<T> {
     f: File,
     _val: T,
 }
 
 impl<T> FileBox<T> where T: Decodable<json::Decoder, json::DecoderError> {
+    /// Creates a new `FileBox` at the given path with the given value. If the file at the path is
+    /// not empty, it will be overwritten.
     pub fn open_new(p: &Path, val: T) -> FileBox<T> {
         FileBox {
             f: File::open_mode(p, io::Truncate, io::Write).unwrap(),
@@ -44,18 +45,28 @@ impl<T> FileBox<T> where T: Decodable<json::Decoder, json::DecoderError> {
         }
     }
 
+    /// Opens a `FileBox` from a file, reading the data stored inside. This will fail if the file
+    /// cannot be read or the file contains invalid data.
     pub fn open(p: &Path) -> FileBox<T> {
         let mut f = File::open_mode(p, io::Open, io::Read).unwrap();
-        let val = json::decode(str::from_utf8(f.read_to_end().unwrap().as_slice()).unwrap()).unwrap();
+        let val = json::decode(str::from_utf8(f.read_to_end().unwrap().as_slice()).unwrap())
+                    .unwrap();
         let f = File::open_mode(p, io::Truncate, io::Write).unwrap();
         FileBox {
             f: f,
             _val: val,
         }
     }
+
+    /// Deletes a `FileBox`, deleting the file it is stored in. Returns the result of deleting the
+    /// file.
+    pub fn delete(self) -> IoResult<()> {
+        fs::unlink(self.f.path())
+    }
 }
 
 impl<T> FileBox<T> where T: Decodable<json::Decoder, json::DecoderError> + Default {
+    /// Creates a new `FileBox` at the given path with its default value.
     pub fn new(p: &Path) -> FileBox<T> {
         FileBox::open_new(p, Default::default())
     }
@@ -76,7 +87,7 @@ impl<T> DerefMut<T> for FileBox<T> {
 #[unsafe_destructor]
 impl<'a, T> Drop for FileBox<T> where T: Encodable<json::Encoder<'a>, IoError> {
     fn drop(&mut self) {
-        // Err... should this *abort* when the file isn’t there?
+        // TODO: decide what this should do if the file can’t be written to
         self.f.write(json::encode(&self._val).as_bytes()).ok().expect("could not write to file");
     }
 }
@@ -87,28 +98,46 @@ mod tests {
 
     #[test]
     fn write_then_read() {
+        let path = Path::new("target/write_then_read");
         {
-            let mut x: FileBox<int> = FileBox::open_new(&Path::new("target/write_then_read"), 10i);
+            let mut x: FileBox<int> = FileBox::open_new(&path, 10i);
             *x += 1i;
         }
-        let x: FileBox<int> = FileBox::open(&Path::new("target/write_then_read"));
+        let x: FileBox<int> = FileBox::open(&path);
         assert_eq!(*x, 11);
     }
 
     #[test]
     fn complex_type() {
+        let path = Path::new("target/complex_type");
         #[deriving(Encodable, Decodable, Default, PartialEq, Show)]
         struct Foo {
             x: String,
             y: (int, f64),
         }
         {
-            let mut x: FileBox<Foo> = FileBox::new(&Path::new("target/complex_type"));
+            let mut x: FileBox<Foo> = FileBox::new(&path);
             *x.y.mut0() += 13;
             *x.y.mut1() -= 3.2;
             x.x.push_str("foo bar");
         }
-        let x: FileBox<Foo> = FileBox::open(&Path::new("target/complex_type"));
+        let x: FileBox<Foo> = FileBox::open(&path);
         assert_eq!(*x, Foo { x: "foo bar".to_string(), y: (13, -3.2) });
+    }
+
+    #[test]
+    #[should_fail]
+    fn delete_box() {
+        let path = Path::new("target/delete_box");
+        let x: FileBox<int> = FileBox::new(&path);
+        match x.delete() {
+            Ok(_) => {
+                // Here it should fail
+                let _: FileBox<int> = FileBox::open(&path);
+            },
+            // We want to do nothing if deleting the file fails, so that the test fails (or rather,
+            // doesn’t)
+            Err(_) => {},
+        }
     }
 }

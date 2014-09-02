a90b627d2f24c16b1fc3565cba49ee36131baac7
P1start <rewi-github@whanau.org>
2014-09-02 16:02:55 +1200

    return `Result` from all the things

--- src/lib.rs
+++ src/lib.rs
@@ -12,12 +12,12 @@
 //! fn main() {
 //!     let path = Path::new("target/filebox.box");
 //!     {
-//!         let mut db = FileBox::open_new(&path, 15i);
+//!         let mut db = FileBox::open_new(&path, 15i).unwrap();
 //!         // The number 15 is now stored in the file "target/filebox.box"
 //!         // and can be retrieved later
 //!         *db += 2;
 //!     }
-//!     let db: FileBox<int> = FileBox::open(&path);
+//!     let db: FileBox<int> = FileBox::open(&path).unwrap();
 //!     println!("{}", *db);
 //! }
 //! ```
@@ -29,6 +29,7 @@ use std::default::Default;
 use std::io::{mod, fs, File, IoError, IoResult};
 use std::fmt::{mod, Show, Formatter};
 use serialize::{Decoder, Decodable, Encoder, Encodable};
+use redox::{DecodeResult, IoError};
 
 /// A box that writes to a file when dropped, and reads from a file when created.
 pub struct FileBox<T> {
@@ -39,23 +40,23 @@ pub struct FileBox<T> {
 impl<'a, T> FileBox<T> where T: Decodable<redox::Decoder<'a>, redox::DecodeError> {
     /// Creates a new `FileBox` at the given path with the given value. If the file at the path is
     /// not empty, it will be overwritten.
-    pub fn open_new(p: &Path, val: T) -> FileBox<T> {
-        FileBox {
-            f: File::open_mode(p, io::Truncate, io::Write).unwrap(),
+    pub fn open_new(p: &Path, val: T) -> DecodeResult<FileBox<T>> {
+        Ok(FileBox {
+            f: try!(File::open_mode(p, io::Truncate, io::Write).map_err(|x| IoError(x))),
             _val: val,
-        }
+        })
     }
 
     /// Opens a `FileBox` from a path, reading the data stored inside. This will fail if the file
     /// cannot be read or the file contains invalid data.
-    pub fn open(p: &Path) -> FileBox<T> {
-        let mut f = File::open_mode(p, io::Open, io::Read).unwrap();
-        let val = redox::Decoder::buffer_decode(f.read_to_end().unwrap()).unwrap();
-        let f = File::open_mode(p, io::Truncate, io::Write).unwrap();
-        FileBox {
+    pub fn open(p: &Path) -> DecodeResult<FileBox<T>> {
+        let mut f = try!(File::open_mode(p, io::Open, io::Read).map_err(|x| IoError(x)));
+        let val = try!(redox::Decoder::buffer_decode(try!(f.read_to_end().map_err(|x| IoError(x)))));
+        let f = try!(File::open_mode(p, io::Truncate, io::Write).map_err(|x| IoError(x)));
+        Ok(FileBox {
             f: f,
             _val: val,
-        }
+        })
     }
 
     /// Deletes a `FileBox`, deleting the file it is stored in. Returns the result of deleting the
@@ -67,13 +68,13 @@ impl<'a, T> FileBox<T> where T: Decodable<redox::Decoder<'a>, redox::DecodeError
 
 impl<'a, T> FileBox<T> where T: Decodable<redox::Decoder<'a>, redox::DecodeError> + Default {
     /// Creates a new `FileBox` at the given path with its default value.
-    pub fn new(p: &Path) -> FileBox<T> {
+    pub fn new(p: &Path) -> DecodeResult<FileBox<T>> {
         FileBox::open_new(p, Default::default())
     }
 
     /// Opens a `FileBox` from a path, creating a new one with a default value if the file doesn’t
     /// exist.
-    pub fn open_or_new(p: &Path) -> FileBox<T> {
+    pub fn open_or_new(p: &Path) -> DecodeResult<FileBox<T>> {
         if p.exists() {
             FileBox::open(p)
         } else {
@@ -116,10 +117,10 @@ mod tests {
     fn write_then_read() {
         let path = Path::new("target/write_then_read");
         {
-            let mut x: FileBox<int> = FileBox::open_new(&path, 10i);
+            let mut x: FileBox<int> = FileBox::open_new(&path, 10i).unwrap();
             *x += 1i;
         }
-        let x: FileBox<int> = FileBox::open(&path);
+        let x: FileBox<int> = FileBox::open(&path).unwrap();
         assert_eq!(*x, 11);
     }
 
@@ -132,27 +133,22 @@ mod tests {
             y: (int, f64),
         }
         {
-            let mut x: FileBox<Foo> = FileBox::new(&path);
+            let mut x: FileBox<Foo> = FileBox::new(&path).unwrap();
             *x.y.mut0() += 13;
             *x.y.mut1() -= 3.2;
             x.x.push_str("foo bar");
         }
-        let x: FileBox<Foo> = FileBox::open(&path);
+        let x: FileBox<Foo> = FileBox::open(&path).unwrap();
         assert_eq!(*x, Foo { x: "foo bar".to_string(), y: (13, -3.2) });
     }
 
     #[test]
-    #[should_fail]
     fn delete_box() {
         let path = Path::new("target/delete_box");
-        let x: FileBox<int> = FileBox::new(&path);
-        match x.delete() {
-            Ok(_) => {
-                // Here it should fail
-                let _: FileBox<int> = FileBox::open(&path);
-            },
-            // We want to do nothing if deleting the file fails, so that the test fails (or rather,
-            // doesn’t)
+        let x: FileBox<int> = FileBox::new(&path).unwrap();
+        x.delete().unwrap();
+        match FileBox::<int>::open(&path) {
+            Ok(_) => fail!("opened the file which should be deleted"),
             Err(_) => {},
         }
     }
@@ -160,10 +156,10 @@ mod tests {
     #[test]
     fn show() {
         let path = Path::new("target/show");
-        let x: FileBox<int> = FileBox::open_new(&path, 1);
+        let x: FileBox<int> = FileBox::open_new(&path, 1).unwrap();
         assert_eq!(format!("{}", x), "1".to_string());
 
-        let x: FileBox<Box<Vec<int>>> = FileBox::open_new(&path, box vec![1, 2, 3]);
+        let x: FileBox<Box<Vec<int>>> = FileBox::open_new(&path, box vec![1, 2, 3]).unwrap();
         assert_eq!(format!("{}", x), "[1, 2, 3]".to_string());
     }
 }
